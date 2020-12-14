0e94ca64b08f73f59e33ace0557a1f30e2cc7f25
Abay Nurpeissov <abaynk123@gmail.com>
2020-12-14 07:41:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const BreakTimer = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='break'>
+      <h1>Break Length</h1>
+      <p>^</p>
     </div>
-  );
+  )
 }
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hello World!</h1>
+        <BreakTimer />
+      </div>
+    );
+  }
+}
+
 
 export default App;
